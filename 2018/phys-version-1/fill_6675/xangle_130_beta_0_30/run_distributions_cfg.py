19c193e9ee87c6fd22a47eefe949b6d9db5d24c7
Mateusz Kocot <matixpl99@gmail.com>
2020-08-27 15:20:07 +0200

    Added fill_6675/xangle_130_beta_0_30

---
+++ 2018/phys-version-1/fill_6675/xangle_130_beta_0_30/run_distributions_cfg.py
@@ -0,0 +1,51 @@
+import FWCore.ParameterSet.Config as cms
+
+from input_files import input_files
+from config import ppsAlignmentConfigESSource
+
+process = cms.Process('testDistributions')
+
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load("CalibPPS.Alignment.ppsAlignmentWorkerTest_cfi")
+process.load("DQMServices.Core.DQMStore_cfi")
+
+# Message Logger
+process.MessageLogger = cms.Service("MessageLogger",
+	destinations = cms.untracked.vstring('cout'),
+	cout = cms.untracked.PSet(
+		threshold = cms.untracked.string('WARNING')
+	)
+)
+
+# load DQM framework
+process.load("DQM.Integration.config.environment_cfi")
+process.dqmEnv.subSystemFolder = "CalibPPS"
+process.dqmSaver.path = ""
+process.dqmSaver.tag = "CalibPPS"
+
+process.source = cms.Source("PoolSource",
+	fileNames = input_files
+)
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(100000))
+
+# Event Setup
+process.ppsAlignmentConfigESSource = ppsAlignmentConfigESSource
+
+process.dqmOutput = cms.OutputModule("DQMRootOutputModule",
+	fileName = cms.untracked.string("dqm_run_distributions_test.root")
+)
+
+process.path = cms.Path(
+  	process.ppsAlignmentWorker
+)
+
+process.end_path = cms.EndPath(
+	process.dqmEnv +
+	process.dqmOutput +
+	process.dqmSaver
+)
+
+process.schedule = cms.Schedule(
+	process.path,
+	process.end_path
+)
