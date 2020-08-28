3ababf67c2a849f4831c95c9be84f6d67986bf20
Mateusz Kocot <matixpl99@gmail.com>
2020-08-28 13:06:36 +0200

    standardised config_fill

--- 2018/phys-version-1/fill_7334/config_fill.py
+++ 2018/phys-version-1/fill_7334/config_fill.py
@@ -6,4 +6,28 @@ ppsAlignmentConfigESSource = cms.ESSource("PPSAlignmentConfigESSource",
 		"x alignment relative",
 		"y alignment"
 	),
+
+	sector_45 = cms.PSet(
+        rp_N = cms.PSet(
+            x_max_mode = cms.double(7.0)
+        ),
+        rp_F = cms.PSet(
+            x_max_mode = cms.double(7.5)
+        ),
+
+        cut_h_c = cms.double(-38.55 + 0.57 - 0.08),
+        cut_v_c = cms.double(1.63 - 2.15 + 0.25)
+    ),
+
+    sector_56 = cms.PSet(
+        rp_N = cms.PSet(
+            x_max_mode = cms.double(7.4)
+        ),
+        rp_F = cms.PSet(
+            x_max_mode = cms.double(8.0)
+        ),
+
+        cut_h_c = cms.double(-39.26 + 0.33),
+        cut_v_c = cms.double(1.49 - 1.80)
+    )
 )
