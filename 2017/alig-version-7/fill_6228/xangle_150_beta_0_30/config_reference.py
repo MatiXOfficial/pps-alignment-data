7d378f8d4408229a9016c7d5a80fdd9b00a119c6
Mateusz Kocot <matixpl99@gmail.com>
2020-09-11 14:00:27 +0200

    Removed 2017 data

--- 2017/alig-version-7/fill_6228/xangle_150_beta_0_30/config_reference.py
+++
@@ -1,7 +0,0 @@
-import sys 
-import os
-import FWCore.ParameterSet.Config as cms
-
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + "/..")
-
-from config_fill_ref import ppsAlignmentConfigESSource
