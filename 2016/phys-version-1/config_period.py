acee1a10435b89b59cbe2d2f08d1a58ed6d2be0d
Mateusz Kocot <mateuszkocot99@gmail.com>
2021-03-28 14:57:05 +0200

    Removed sequence from the configuration files.

--- 2016/phys-version-1/config_period.py
+++ 2016/phys-version-1/config_period.py
@@ -1,12 +1,6 @@
 import FWCore.ParameterSet.Config as cms
 
 ppsAlignmentConfigESSource = cms.ESSource("PPSAlignmentConfigESSource",
-	sequence = cms.vstring(
-		"x_alignment",
-		"x_alignment_relative",
-		"y_alignment"
-	),
-	
 	sector_45 = cms.PSet(
 		rp_N = cms.PSet(
 			name = cms.string("L_1_N"),
